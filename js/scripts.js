8e1295a375db26221d433457bb0827216c3825e7
Chris Owens <guitarrockstarco@gmail.com>
2020-08-19 11:22:33 -0400

    Add functionality to buttom to create an unordered list with items

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const button = document.querySelector('#generateList');
+const inputs = document.querySelectorAll(".input")
+const myList = document.querySelector('#myList')
+const ul = document.createElement('ul');
+
+myList.appendChild(ul)
+
+button.addEventListener('click', (e) => {
+    ul.innerHTML = "";
+    inputs.forEach((item) => {
+        if (item.value) {
+            const list = document.createElement("li")
+            ul.appendChild(list);
+            list.innerHTML = item.value;
+        }
+    })
+})
